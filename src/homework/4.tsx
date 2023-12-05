c04ac2389d5ec4fb5ae33df774e40b975cd729fd
Ruslan <Giovingo92@gmail.com>
2023-12-05 13:59:19 +0200

    fix: task-04

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -7,7 +7,7 @@ type Menu = { id: MenuIds; title: string };
 // Додати тип Menu Selected
 
 interface SelectedMenu {
-  id?: MenuIds;
+  id: MenuIds;
 }
 
 interface MenuSelected {
@@ -19,7 +19,7 @@ interface MenuAction {
 }
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: {} as SelectedMenu,
 });
 
 // Додайте тип MenuAction
@@ -34,7 +34,9 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>(
+    {} as SelectedMenu
+  );
 
   const menuContextAction = useMemo(
     () => ({
